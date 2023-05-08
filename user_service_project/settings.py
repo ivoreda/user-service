40ff0ce70f5e2d3f6094ac7e752df5e32c358369
ivoreda <ivoredafej@gmail.com>
2023-05-08 03:17:13 +0100

    Added new db, first deployment to railway

--- user_service_project/settings.py
+++ user_service_project/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = "user_service_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD': '8rCvFG2j9rqAV16QXRhy',
+        'HOST': 'containers-us-west-157.railway.app',
+        'PORT': 6061,
     }
 }
 
