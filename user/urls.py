95b177acaf6e38253c4c3559b4f8b40337136c74
ivoreda <ivoredafej@gmail.com>
2023-05-26 16:34:21 +0100

    added preceding slash to urls path

--- user/urls.py
+++ user/urls.py
@@ -7,18 +7,18 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('login', TokenObtainPairView.as_view(), name='login'),
-    path('refresh-token', TokenRefreshView.as_view(), name='refresh-token'),
-    path('login', views.LoginView.as_view()),
-    path('signup', views.SignupView.as_view()),
-    path('get-user', views.GetUserView.as_view()),
-    path('update-user-hobbies', views.UpdateHobbiesView.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh-token/', TokenRefreshView.as_view(), name='refresh-token'),
+    path('login/', views.LoginView.as_view()),
+    path('signup/', views.SignupView.as_view()),
+    path('get-user/', views.GetUserView.as_view()),
+    path('update-user-hobbies/', views.UpdateHobbiesView.as_view()),
     # path('update-profile-picture', views.UpdateProfilePictureView.as_view()),
 
-    path('verify-email', views.EmailVerificationView.as_view()),
+    path('verify-email/', views.EmailVerificationView.as_view()),
     path('send-email-verification-code/', views.SendEmailVerificationCodeView.as_view(),
          name='send-email-verification-code'),
-    path('forgot-password', views.ForgotPasswordView.as_view()),
-    path('reset-password', views.ResetPasswordView.as_view()),
-    path('change-password', views.ChangePasswordView.as_view()),
+    path('forgot-password/', views.ForgotPasswordView.as_view()),
+    path('reset-password/', views.ResetPasswordView.as_view()),
+    path('change-password/', views.ChangePasswordView.as_view()),
 ]
